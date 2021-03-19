2767dccfcf771ee0d1c3460672e9cac82d990a43
leonardoaguirre <62442754+leonardoaguirre@users.noreply.github.com>
2021-03-18 21:19:09 -0300

    trocando useForm

--- web/src/pages/userForm/index.tsx
+++ web/src/pages/userForm/index.tsx
@@ -1,6 +1,6 @@
 import PageFooter from '../../components/PageFooter';
 import PageHeaderAdministration from '../../components/PageHeaderAdministration';
-import styles from '../../styles/pages/ProductForm.module.css';
+import styles from '../../styles/pages/UserForm.module.css';
 
 function userForm() {
     return (
@@ -10,6 +10,18 @@ function userForm() {
                 <h1>Cadastrar-se</h1>
                 <div className={styles.formContainer}>
                     <form>
+                        <div className={styles.email}>
+                            <label htmlFor="email">Email: </label>
+                            <div className={styles.inputContainer}>
+                                <input type="text" placeholder="Email" />
+                            </div>
+                        </div>
+                        <div className={styles.password}>
+                            <label htmlFor="password">Senha: </label>
+                            <div className={styles.inputContainer}>
+                                <input type="password" placeholder="Senha" />
+                            </div>
+                        </div>
                         <div className={styles.name}>
                             <label htmlFor="name">Nome: </label>
                             <div className={styles.inputContainer}>
@@ -31,7 +43,13 @@ function userForm() {
                         <div className={styles.birthDate}>
                             <label htmlFor="dataNascimento">Data de nascimento: </label>
                             <div className={styles.inputContainer}>
-                                <input type="text" placeholder="Data de nascimento" />
+                                <input type="date" placeholder="Data de nascimento" />
+                            </div>
+                        </div>
+                        <div className={styles.telephone}>
+                            <label htmlFor="telephone">Telefone: </label>
+                            <div className={styles.inputContainer}>
+                                <input type="text" placeholder="(XX) xxxx-xxx" />
                             </div>
                         </div>
                         <div className={styles.buttonsContainer}>
