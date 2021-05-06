91f90a8b325e171ea94ffdd4c01d6b8fb3db73f5
Pedro Mitinari Yamamoto <pedro76272@gmail.com>
2021-05-06 20:50:33 -0300

    Adicionando Banner
    (Carousel)

---
+++ web/src/components/Banner.tsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
+interface BannerProps {
+    source: string[];
+}
+
+const Banner: React.FC<BannerProps> = (props) => {
+
+    return (
+        <Carousel autoPlay={true} infiniteLoop={true}>
+            
+            {props.source.map((source, index) => {
+                return <div><img src={`${source}`} key={index}/></div>
+            })}
+            
+        </Carousel>
+    );
+};
+
+
+export default Banner;
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,21 +1,28 @@
 import PageHeader from '../components/PageHeader';
 import PageFooter from '../components/PageFooter';
 import SubHeader from '../components/SubHeader';
+import Banner from '../components/Banner';
 import Head from 'next/head';
 import styles from '../styles/pages/Home.module.css';
 import * as dotenv from 'dotenv';
 
 export default function Home() {
   //dotenv.config({path : "../.env.local"});
+  const images = ['images/1.png', 'images/2.jpg', 'images/3.jpg']
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} >
       <Head>
         <title>Início | Ferragens Pérola do Vale</title>
       </Head>
       <PageHeader />
       <SubHeader />
       <div className={styles.home}>
-        
+        Promoção
+        <Banner source={images}/>
+        <Banner source={images}/>
+        <Banner source={images}/>
+        <Banner source={images}/>
       </div>
       <PageFooter />
     </div>
