2fa537837b48d0d27a51d4607e949a82a1bb5374
Carlos Hideki Yamakawa <hideki.yamakawa@outlook.com>
2021-11-16 22:11:08 -0300

    Criação de variaveis de ambiente no next.config.js

--- web/src/pages/teste/index.tsx
+++ web/src/pages/teste/index.tsx
@@ -1,18 +1,21 @@
 import { useState } from 'react';
 
 interface TesteProps {
-  
+
 }
 
 const Teste: React.FC<TesteProps> = ({
-  
+
 }) => {
+
+  console.log(process.env.production)
 
   const [valor, setValor] = useState<number>(20.90);
 
   return (
     <>
-      <button onClick={() => setValor(valor + 10.00)}>+</button>
+      <h1>TYPE: {process.env.production}</h1>
+      <h2>URL: {process.env.API}</h2>
     </>
   );
 }
