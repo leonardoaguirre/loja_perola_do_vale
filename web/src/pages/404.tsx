27ab4c669995cfad3f15c68d4e580a38332476e2
Carlos Hideki Yamakawa <hideki.yamakawa@outlook.com>
2021-11-26 14:53:11 -0300

    Estilização da tela de 404

--- web/src/pages/404.tsx
+++ web/src/pages/404.tsx
@@ -1,24 +1,31 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import { FaRegSadCry } from 'react-icons/fa';
 
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import styles from './404.module.css';
+import { Button } from 'react-bootstrap';
+import { IoIosArrowBack } from 'react-icons/io';
 
 function Error404() {
   return (
     <div className="pageContainer">
       <Head><title>Erro 404 | Ferragens Pérola do Vale</title></Head>
       <Header />
-      <div id={styles.error404} className="pageContent fx-row just-cont-center align-i-center">
-        <img src="/images/sad_face.png" alt="Sad face emoji" />
-        <div className={styles.rightContainer}>
-          <h1>Erro 404!</h1>
-          <h2>Ops, a página que você pesquisou não existe ou já expirou...</h2>
-          <br />
-          <Link href="/">
-            <a>Voltar para a página inícial <img src="/icons/double_arrow_blue_36dp.png" alt="seta dupla" /></a>
-          </Link>
+      <div className="pageContent">
+        <div id={styles.error404}>
+          <div className={styles.content}>
+            <FaRegSadCry />
+            <div className={styles.right}>
+              <h2>Ops, a página que você pesquisou não existe ou já expirou...</h2>
+              <Link href={'/'}>
+                <a>
+                  <Button id={styles.backToBuy} variant="primary"><IoIosArrowBack />Voltar à página inicial</Button>
+                </a>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
       <Footer />
