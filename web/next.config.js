0273fd3e7b5fac8373257e3a75bef2af9d86004c
Carlos Hideki Yamakawa <hideki.yamakawa@outlook.com>
2021-11-28 16:25:33 -0300

    Redireção após login aprimorada

--- web/next.config.js
+++ web/next.config.js
@@ -1,17 +1,19 @@
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
 module.exports = (phase, { defaultConfig }) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
       env: {
         production: false,
-        API: 'http://localhost:3008',
-      }
-    }
+        API: "http://localhost:3008", // API url
+        URL: "http://localhost:3000", // interface/frontend url
+      },
+    };
   }
 
   return {
     production: true,
-    API: 'http://localhost:3008'
-  }
-}
+    API: "http://localhost:3008", // API url
+    URL: "http://localhost:3000", // interface/frontend url
+  };
+};
