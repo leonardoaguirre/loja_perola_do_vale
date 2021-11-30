5286aecb15740a21f3a30dbe4513f6c9f4a70ecf
Carlos Hideki Yamakawa <hideki.yamakawa@outlook.com>
2021-11-29 23:57:35 -0300

    Adição de paginação na tela de gerenciamento de fornecedores

--- web/src/components/ProviderTable/index.tsx
+++ web/src/components/ProviderTable/index.tsx
@@ -1,9 +1,13 @@
-import Link from "next/link";
-import React, { useState } from "react";
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
+import { Table } from 'react-bootstrap';
+import { FaPen, FaTrash } from 'react-icons/fa';
 
-import styles from "./styles.module.css";
-import api from '../../services/api';
+import { useToasts } from '../../contexts/ToastContext';
 import { Provider } from '../../models/Provider';
+import api from '../../services/api';
+import { ModalExclusion } from '../Modal';
+import styles from './styles.module.css';
 
 
 interface ProviderTableProps {
@@ -11,26 +15,42 @@ interface ProviderTableProps {
 }
 
 const ProviderTable: React.FC<ProviderTableProps> = (props) => {
+
+  const router = useRouter();
+  const { addToast } = useToasts();
   const [lineSelected, setLineSelected] = useState(null);
+  const [showExclusionModal, setShowExclusionModal] = useState<boolean>(false)
 
   const selecionaLinha = async (event) => {
-    const linha: HTMLElement = event.target.parentNode;
 
-    if (linha.tagName == "TR") {
-      if (!linha.className) {
-        if (lineSelected) {
-          document
-            .getElementsByClassName(`${styles.selected}`)[0]
-            .removeAttribute("class");
-        }
-        linha.className += ` ${styles.selected}`;
-        setLineSelected(linha);
-      } else {
-        linha.removeAttribute("class");
-        setLineSelected(null);
+    const linha: HTMLElement = findTr(event.target);
+
+    if (!linha.className) {
+      if (lineSelected) {
+        document
+          .getElementsByClassName(`${styles.selected}`)[0]
+          .removeAttribute("class");
       }
+      linha.className += ` ${styles.selected}`;
+      setLineSelected(linha);
+    } else {
+      linha.removeAttribute("class");
+      setLineSelected(null);
     }
   };
+
+  const findTr = (el) => {
+    const parentNode: HTMLElement = el.parentNode;
+    if (parentNode.tagName == "TR") {
+      // TR encontrado
+      return parentNode;
+    } else {
+      // Ainda não é o TR
+      console.log(parentNode)
+      const trEl = findTr(parentNode);
+      return trEl;
+    }
+  }
 
   const removeLinha = async (trIndex: number) => {
     var el: any = document.getElementById("table");
@@ -38,84 +58,241 @@ const ProviderTable: React.FC<ProviderTableProps> = (props) => {
     setLineSelected(null);
   };
 
-  const deleteUser = (userid: string, trIndex: number) => {
-    console.log(userid)
+  const deleteUser = (providerId: string, trIndex: number) => {
     api.delete("Fornecedor/Deletar", {
       data: {
-        id: userid
+        id: providerId
       }
     }).then(
       (res) => {
         if (res.status === 200) {
           removeLinha(trIndex);
+          addToast({
+            title: 'Fornecedor deletado',
+            content: `O fornecedor foi deletado com sucesso!`,
+            delay: 8000,
+            autohide: true,
+          })
         } else {
-          console.log("Falha ao deletar fornecedor");
+          addToast({
+            title: 'Falha',
+            content: `Falha ao tentar deletar o fornecedor`,
+            delay: 8000,
+            autohide: true,
+          })
         }
       }
     ).catch(
       (error) => {
-        console.log(error);
+        addToast({
+          title: 'Falha',
+          content: `${error ? error : `Erro ao tentar deletar`}`,
+          delay: 8000,
+          autohide: true,
+        })
       }
     )
+    setShowExclusionModal(false);
+  }
+
+  const onClickButton = (url: string) => {
+    router.push(url);
   }
 
   return (
-    <div className={styles.providerTable}>
-      <table id="table" className={styles.table}>
+    <div className={styles.clientTable}>
+      <Table responsive striped bordered hover id={styles.table}>
         <thead>
           <tr>
             <th>Nome Fantasia</th>
             <th>CNPJ</th>
             <th>Email</th>
-            <th>Telefone</th>
-            <th>Endereço</th>
+            {/* <th>Telefone</th> */}
+            {/* <th>Endereço</th> */}
           </tr>
         </thead>
-        <tbody onClick={selecionaLinha}>
+        <tbody onMouseDown={selecionaLinha}>
           {props.providers.map((provider, index) => (
             <tr key={index}>
               <td>{provider.pessoaJuridica.nomeFantasia}</td>
               <td>{provider.pessoaJuridica.cnpj}</td>
               <td>{provider.pessoaJuridica.pessoa.email}</td>
-              {/* <td>{provider.pessoaJuridica.pessoa.telefones[0]}</td> // TODO: listar telefones */}
-              {/* <td>{provider.pessoaJuridica.pessoa.enderecos[0]}</td> // TODO: listar enderecos */}
+              {/* <td>{provider.pessoaJuridica.pessoa?.telefones[0]}</td> // TODO: listar telefones */}
+              {/* <td>{provider.pessoaJuridica.pessoa?.enderecos[0]}</td> // TODO: listar enderecos */}
             </tr>
           ))}
         </tbody>
-      </table>
-      <div className={styles.actionsContainer}>
-        <div className={styles.buttonContainer}>
-          <button
-            id="deletebtn"
-            className={styles.deleteButton}
-            onClick={() =>
-              deleteUser(props.providers[lineSelected.rowIndex - 1].id, lineSelected.rowIndex)
-            }
-            disabled={!lineSelected}
-          >
-            Excluir
-          </button>
-          <a
-            href={
-              lineSelected
-                ? `/adm/manage/provider/Form/${props.providers[lineSelected.rowIndex - 1].id
-                }`
-                : ""
-            }
-          >
-            <button id="updatebtn" className={styles.updateButton} disabled={!lineSelected}>
-              Alterar
-            </button>
-          </a>
-          <Link href="/">
-            <a>
-              <button className={styles.createButton}>Cadastrar</button>
-            </a>
-          </Link>
+      </Table>
+      <div className={styles.bContainer}>
+        <div className={styles.aContainer}>
+          <div className={styles.actions}>
+            <div className={styles.hover}>
+              <div className={styles.edit}>
+                <button
+                  title="Editar"
+                  className={lineSelected ? styles.active : styles.disabled}
+                  disabled={!lineSelected}
+                  onClick={() =>
+                    onClickButton(`/adm/manage/provider/form/${props.providers[lineSelected.rowIndex - 1].id}`)
+                  }
+                >
+                  <FaPen />
+                </button>
+              </div>
+              <div className={styles.delete}>
+                <button
+                  title="Excluir"
+                  id="deletebtn"
+                  className={lineSelected ? styles.active : styles.disabled}
+                  onClick={() =>
+                    setShowExclusionModal(true)
+                  }
+                  disabled={!lineSelected}
+                >
+                  <FaTrash />
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+      {showExclusionModal ?
+        <ModalExclusion
+          objN="Cliente"
+          show={showExclusionModal}
+          onConfirm={(e) => deleteUser(props.providers[lineSelected.rowIndex - 1].id, lineSelected.rowIndex)}
+          onHide={() => setShowExclusionModal(false)}
+        >
+          Você realmente deseja excluir o fornecedor <span className="bold">{props.providers[lineSelected.rowIndex - 1].pessoaJuridica.nomeFantasia}</span>?
+        </ModalExclusion>
+        : ''
+      }
     </div>
   );
 };
 
 export default ProviderTable;
+
+
+
+// import Link from 'next/link';
+// import React, { useState } from 'react';
+
+// import { Provider } from '../../models/Provider';
+// import api from '../../services/api';
+// import styles from './styles.module.css';
+
+
+// interface ProviderTableProps {
+//   providers: Provider[];
+// }
+
+// const ProviderTable: React.FC<ProviderTableProps> = (props) => {
+//   const [lineSelected, setLineSelected] = useState(null);
+
+//   const selecionaLinha = async (event) => {
+//     const linha: HTMLElement = event.target.parentNode;
+
+//     if (linha.tagName == "TR") {
+//       if (!linha.className) {
+//         if (lineSelected) {
+//           document
+//             .getElementsByClassName(`${styles.selected}`)[0]
+//             .removeAttribute("class");
+//         }
+//         linha.className += ` ${styles.selected}`;
+//         setLineSelected(linha);
+//       } else {
+//         linha.removeAttribute("class");
+//         setLineSelected(null);
+//       }
+//     }
+//   };
+
+//   const removeLinha = async (trIndex: number) => {
+//     var el: any = document.getElementById("table");
+//     el.deleteRow(trIndex);
+//     setLineSelected(null);
+//   };
+
+//   const deleteUser = (userid: string, trIndex: number) => {
+//     console.log(userid)
+//     api.delete("Fornecedor/Deletar", {
+//       data: {
+//         id: userid
+//       }
+//     }).then(
+//       (res) => {
+//         if (res.status === 200) {
+//           removeLinha(trIndex);
+//         } else {
+//           console.log("Falha ao deletar fornecedor");
+//         }
+//       }
+//     ).catch(
+//       (error) => {
+//         console.log(error);
+//       }
+//     )
+//   }
+
+//   return (
+//     <div className={styles.providerTable}>
+//       <table id="table" className={styles.table}>
+//         <thead>
+//           <tr>
+//             <th>Nome Fantasia</th>
+//             <th>CNPJ</th>
+//             <th>Email</th>
+//             <th>Telefone</th>
+//             <th>Endereço</th>
+//           </tr>
+//         </thead>
+//         <tbody onClick={selecionaLinha}>
+//           {props.providers.map((provider, index) => (
+//             <tr key={index}>
+//               <td>{provider.pessoaJuridica.nomeFantasia}</td>
+//               <td>{provider.pessoaJuridica.cnpj}</td>
+//               <td>{provider.pessoaJuridica.pessoa.email}</td>
+//               {/* <td>{provider.pessoaJuridica.pessoa.telefones[0]}</td> // TODO: listar telefones */}
+//               {/* <td>{provider.pessoaJuridica.pessoa.enderecos[0]}</td> // TODO: listar enderecos */}
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+//       <div className={styles.actionsContainer}>
+//         <div className={styles.buttonContainer}>
+//           <button
+//             id="deletebtn"
+//             className={styles.deleteButton}
+//             onClick={() =>
+//               deleteUser(props.providers[lineSelected.rowIndex - 1].id, lineSelected.rowIndex)
+//             }
+//             disabled={!lineSelected}
+//           >
+//             Excluir
+//           </button>
+//           <a
+//             href={
+//               lineSelected
+//                 ? `/adm/manage/provider/Form/${props.providers[lineSelected.rowIndex - 1].id
+//                 }`
+//                 : ""
+//             }
+//           >
+//             <button id="updatebtn" className={styles.updateButton} disabled={!lineSelected}>
+//               Alterar
+//             </button>
+//           </a>
+//           <Link href="/">
+//             <a>
+//               <button className={styles.createButton}>Cadastrar</button>
+//             </a>
+//           </Link>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ProviderTable;
--- web/src/pages/adm/manage/providers/search/index.tsx
+++ web/src/pages/adm/manage/providers/search/index.tsx
@@ -1,8 +1,11 @@
 import Head from 'next/head';
-import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { Button, Col, Container, Row, Spinner } from 'react-bootstrap';
+import { MdAddCircle } from 'react-icons/md';
 
-import Footer from '../../../../../components/Footer';
 import Header from '../../../../../components/Header';
+import PaginationBarSR from '../../../../../components/PaginationBarSR';
 import ProviderTable from '../../../../../components/ProviderTable';
 import SearchBox from '../../../../../components/SearchBox';
 import { Provider } from '../../../../../models/Provider';
@@ -13,39 +16,41 @@ interface PageUserProps {
   providers: Provider[];
 }
 
+interface SearchProps {
+  providers: Provider[];
+  nPages: number;
+}
+
 const PageUser: React.FC<PageUserProps> = (props) => {
 
-  const [isActive, setIsActive] = useState<boolean>(false);
-  const [tableData, setTableData] = useState<Provider[]>(null);
+  const router = useRouter();
+
+  const [tableData, setTableData] = useState<SearchProps>(null);
   const [error, setError] = useState<string>('');
 
-  // const reloadTable = async () => {
-  //   const tokenCookie = Cookies.get("tokenCookie");
+  const [activePage, setActivePage] = useState<number>(1)
+  const [search, setSearch] = useState<string>('')
+  const [atribute, setAtribute] = useState<string>('');
 
-  //   const pessoa = { headers: { 'authorization': tokenCookie }, method: "GET" };
-
-  //   const response = await fetch(`${environment.API}/Cliente/listar`, pessoa);
-
-  //   setTableItens(await response.json());
-  // }
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const filterOptions = [
-    {value: "nomeFantasia", viewValue: "Nome Fantasia"}, 
-    {value: "email", viewValue: "Email"}, 
-    {value: "cnpj", viewValue: "CNPJ"},
+    { value: "nome", viewValue: "Nome Fantasia" },
+    { value: "email", viewValue: "Email" },
+    { value: "cnpj", viewValue: "CNPJ" },
   ];
 
   const handleSearch = async (searchStr: string, atribute: string) => {
     if (searchStr.length > 0) {
+      setIsLoading(true);
       await api.get(`Fornecedor/Buscar/${atribute}/${searchStr}`)
         .then(
           (res) => {
             if (res.status === 201) {
               setTableData(res.data);
-              setIsActive(true);
               setError('');
             } else {
-              setTableData([new Provider]);
+              setTableData({ providers: [], nPages: 0 });
               setError(res.data.constraints.message);
             }
           }
@@ -54,28 +59,69 @@ const PageUser: React.FC<PageUserProps> = (props) => {
             setError('Nenhum resultado encontrado');
             setTableData(null)
           }
-        )
+        ).finally(() => setIsLoading(false));
     } else {
       setError('Campo de pesquisa está vazio');
     }
+  }
+
+  useEffect(() => {
+    if (search) handleSearch(search, atribute) // faz a pesquisa somente quando o activePage muda
+  }, [activePage])
+
+
+  const onClickButton = (url: string, event: any) => {
+    router.push(url);
   }
 
   return (
     <div className="pageContainer">
       <Head><title>Buscar Fornecedores | Ferragens Pérola do Vale</title></Head>
       <Header />
-      <div className={styles.providerSearch}>
-        <SearchBox
-          filterOptions={filterOptions}
-          handleSearch={handleSearch}
-          error={error}
-        />
-        {tableData ?
-          <ProviderTable providers={tableData} />
-          : ''
-        }
+      <div className="pageContent">
+        <h2>Gerenciamento de Fornecedores</h2>
+        <Container fluid>
+          <Row className="pb-4">
+            <Col xs={9} lg={10}>
+              <SearchBox
+                filterOptions={filterOptions}
+                handleSearch={handleSearch}
+                emitHandleInputChange={true}
+                handleInputChange={setSearch}
+                handleSelectChange={setAtribute}
+                error={error}
+                placeholder="Digite sua pesquisa aqui"
+              />
+            </Col>
+            <Col xs={3} lg={2}>
+              <Button variant="success" className={styles.createButton} onClick={(event) => onClickButton('/adm/manage/user/customer/form', event)}>
+                <MdAddCircle />
+                <p>Cadastrar</p>
+              </Button>
+            </Col>
+          </Row>
+          {(isLoading ? (
+            <Row>
+              <Col className="d-flex justify-content-center pt-5" xs={12}>
+                <Spinner id={styles.spinner} animation="border" role="status" variant="primary">
+                  <span className="visually-hidden">Loading...</span>
+                </Spinner>
+              </Col>
+            </Row>
+          ) : (
+            (tableData ?
+              <>
+                <Row>
+                  <ProviderTable providers={tableData.providers} />
+                </Row>
+                <Row>
+                  <PaginationBarSR nPages={tableData.nPages} activePage={activePage} onClick={setActivePage} />
+                </Row>
+              </>
+              : '')
+          ))}
+        </Container>
       </div>
-      <Footer />
     </div>
   );
 }
